4e518548ac3f026c282b0aa6a820c048a663f756
liwei333 <1606308805@qq.com>
2019-09-12 23:15:05 +0800

    lesson4

---
+++ lesson4proxy-obj-nesting/core/instance/proxy.js
@@ -0,0 +1,59 @@
+function construcObjectProxy(vm,obj,namespace){
+    let proxyObj = {};
+    for(let prop in obj){
+        Object.defineProperty(proxyObj,prop,{
+            configurable:true,
+            get(){
+               return obj[prop];
+            },
+            set:function(vaule){
+                console.log(getNameSpace(namespace,prop));
+                obj[prop]=vaule;
+            }
+        })
+        Object.defineProperty(vm,prop,{
+            configurable:true,
+            get(){
+               return obj[prop];
+            },
+            set:function(vaule){
+                console.log(getNameSpace(namespace,prop));
+                obj[prop]=vaule;
+            }
+        });
+        if(obj[prop] instanceof Object){
+            proxyObj[prop] = construcProxy(vm,obj[prop],getNameSpace(namespace,prop));
+        }
+        
+    }
+
+    return proxyObj;
+
+}
+//我们要知道哪个属性被修改了，我们才能对页面上的内容进行更新
+//所以我们必须先能够捕获修改的这个事件
+//所以我们需要用代理的方式来监听
+export function construcProxy(vm,obj,namespace){
+   
+    //vm表示Due对象，obj表示进行代理的对象，namespace表示命名空间
+    //递归
+    let proxyObj = null;
+    if(obj instanceof Array){
+
+    }else if(obj instanceof Object){
+        proxyObj = construcObjectProxy(vm,obj,namespace);
+    }else{
+        throw new Error("error");
+    }
+    return proxyObj;
+}
+
+function getNameSpace(nowNameSpace,nowProp){
+    if(nowNameSpace = null || nowNameSpace==""){
+        return nowProp;
+    }else if(nowProp = null || nowProp==""){
+        return nowNameSpace;
+    }else{
+        return nowNameSpace + "." + nowProp;
+    }
+}
